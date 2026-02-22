2b07202cf54a842cfdae58665464a4922a694065
L3SODB <joannakamikazi32@gmail.com>
2026-02-22 01:20:48 -1000

    Joanna,s Portfolio

--- joanna_Portfolio/src/App.vue
+++ joanna_Portfolio/src/App.vue
@@ -3,6 +3,8 @@ import Footer from './components/Footer.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import { ref } from 'vue';
 const userName = ref('Joanna Kamikazi');
+import {ref} from 'vue';
+const show = ref(true);
 </script>
 <template>
   <h1>Welcome to Kamikazi's Portfolio</h1>
@@ -10,4 +12,7 @@ const userName = ref('Joanna Kamikazi');
   <input v-model="userName" placeholder="Enter your name" />
   <HeaderComponent />
   <Footer />
+  <button @click="show = !show">Toggle</button>
+  <p v-if="show">Visible using v-if!</p>
+  <p v-show="show">Visible using v-show</p>
 </template>
